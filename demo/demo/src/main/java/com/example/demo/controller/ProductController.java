0299d96b7580cbd660193f9706ebc429a0e240d7
yoolalw <larakemilyprofissional@gmail.com>
2026-03-09 22:41:38 -0300

    finalizando

--- demo/demo/src/main/java/com/example/demo/controller/ProductController.java
+++ demo/demo/src/main/java/com/example/demo/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.example.demo.service.ProductService;
 
 @RestController
 @RequestMapping("/produtos")
+@CrossOrigin(origins = "*")
 public class ProductController {
     @Autowired
     private ProductService productService;
